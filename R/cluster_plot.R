6481ac7d56e66ea8c70ce94de462f329eb67f871
NiloofarJalali <niloofar_jalali@hms.harvard.edu>
2018-04-12 15:53:56 -0400

    modify cluster function

--- R/cluster_plot.R
+++ R/cluster_plot.R
@@ -1,18 +1,18 @@
- #' Plot of clusters
-#' @param L  The list of different clusters
+#' Plot of clusters
+#' @param clusterList  The list of different clusters
 #' @param ClusterSubject  vector of disease symptoms that are used fro clustering
 #' @param n the minimum prevalence threshold
 
 #' @examples
 #' example1 <- cluster_plot(
-#'                       L               =list(subgroup1,subgroup2,subgroup3)
-#'                       ClusterSubject  =GI$phenotypes
+#'                       clusterList     =clusterList
+#'                       ClusterSubject  =clusterSubject
 #'                       n               =5
 #'                     )
 #'
 #'  @export cluster_plot
 
-cluster_plot=function(L,ClusterSubject,n){
+cluster_plot=function(clusterList,ClusterSubject,n){
 
    library(ggplot2)
    Gplot=function(input,ClusterSubject,n){
@@ -25,7 +25,7 @@ cluster_plot=function(L,ClusterSubject,n){
     names(d)[3]="Prevalence"
     return(d)}
 
-  xdG=lapply(L,function(x) Gplot(x,ClusterSubject,n))
+  xdG=lapply(clusterList,function(x) Gplot(x,ClusterSubject,n))
 
   for (i in 1:length(xdG)){
     b=xdG[[i]]
@@ -35,7 +35,7 @@ cluster_plot=function(L,ClusterSubject,n){
     assign(name2,b)
   }
 
-  E=Reduce("rbind", mget(paste0("T", 1:length(L))))
+  E=Reduce("rbind", mget(paste0("T", 1:length(clusterList))))
   E=E[,c(1,3,4)]
   p=ggplot(E, aes(fill=condition, y=Prevalence, x=Phenotype))+geom_bar(position= position_dodge(preserve ="single"),stat="identity",width = 0.9) +geom_text(size=4,aes(label=Prevalence), position = position_dodge(width = 0.5),vjust =-0.5, size = 1.5 ,hjust=0.8, inherit.aes = TRUE)
   p+  theme_bw()+theme(axis.text.x = element_text(size=12,angle = 75, hjust = 1),axis.title = element_text(size=12,face="bold"),legend.text = element_text(size=12)) + scale_fill_manual(values=c("#00BFFF","#DAA520", "#ADFF2F","#2F4F4F","#EEE0E5"))
